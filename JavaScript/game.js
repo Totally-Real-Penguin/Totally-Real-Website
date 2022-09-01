e0954ded3e502e5407c9ea0aff29b67c87aaf8b4
Totally Real Penguin <111720064+Totally-Real-Penguin@users.noreply.github.com>
2022-09-01 22:08:38 +0100

    added old stuff

---
+++ JavaScript/game.js
@@ -0,0 +1,10 @@
+function esr() {
+    document.getElementById("game").src = "https://itch.io/embed-upload/6381747?color=483D8B";
+    document.getElementById("esr").className = "active";
+    document.getElementById("pdhk").className = "";
+}
+function pdhk() {
+    document.getElementById("game").src = "https://itch.io/embed-upload/6381633?color=483D8B";
+    document.getElementById("pdhk").className = "active";
+    document.getElementById("esr").className = "";
+}
---
+++ JavaScript/secret.js
@@ -0,0 +1,5 @@
+const rndInt = Math.floor(Math.random() * 10) + 1
+
+if (rndInt === 1) {
+    document.getElementById("secret").innerHTML = "super secret";
+}
