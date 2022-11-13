f5480fe4c217ba072f860058aecc4baece30a4a6
Totally Real Penguin <johnthomson190921@gmail.com>
2022-11-13 22:26:40 +0000

    updated design and information

--- JavaScript/game.js
+++ JavaScript/game.js
@@ -1,10 +1,10 @@
 function esr() {
-    document.getElementById("game").src = "https://itch.io/embed-upload/6381747?color=483D8B";
+    document.getElementById("game").src = "https://itch.io/embed-upload/6381747?color=222222";
     document.getElementById("esr").className = "active";
     document.getElementById("pdhk").className = "";
 }
 function pdhk() {
-    document.getElementById("game").src = "https://itch.io/embed-upload/6381633?color=483D8B";
+    document.getElementById("game").src = "https://itch.io/embed-upload/6381633?color=222222";
     document.getElementById("pdhk").className = "active";
     document.getElementById("esr").className = "";
 }
